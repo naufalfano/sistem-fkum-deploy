00b2a5fcd5a60b10beda073d10a6f155a93d5b1e
naufalfano <naufal.fano@gmail.com>
2024-12-01 00:39:06 +0700

    add form ukmppd

--- dataUKMPPD/views.py
+++ dataUKMPPD/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def dashboardUKMPPD(request):
     return render(request, 'dataUKMPPD/dashboard-ukmppd.html')
+
+def uploadUKMPPD(request):
+    return render(request, 'dataUKMPPD/form-upload-ukmppd.html')
