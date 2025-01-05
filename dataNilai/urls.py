d5461528221b64a86e57ca7b9630d411c0206861
naufalfano <naufal.fano@gmail.com>
2025-01-05 22:24:17 +0700

    detail

--- dataNilai/urls.py
+++ dataNilai/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('detail-nilai/', views.detailNilai),
+    path('detail-nilai/<str:NIM>', views.detailNilai, name='detail-nilai'),
     path('form-nilai/', views.uploadNilai, name='form-upload-nilai'),
     path('upload-nilai', views.upload_nilai, name='upload-nilai'),
 ]
