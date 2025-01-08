d54c1867f2000d2fe95eb3e7636276400c4474f0
naufalfano <naufal.fano@gmail.com>
2025-01-08 21:19:36 +0700

    model

--- modelPrediksi/views.py
+++ modelPrediksi/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from dataUKMPPD.models import hasilUKMPPD
 
 def modelPrediksi(request):
     return render(request, 'modelPrediksi/dashboard-model.html')
+
+def retrain_sem_2(request):
+    #Ambil data training dari database
+    X = hasilUKMPPD.objects.values('IPD', 'IKA', 'RAD', 'SRM', 'KDK', 'MPK', 'ANT', 'MAT', 'IKM', 'THTKL', 'KJW', 'OT2')
+    y = hasilUKMPPD.objects.values('hasil_ukmppd')
+    return render(request, 'modelPrediksi/retrain-sem-2.html')
+
+def retrain_sem_3(request):
+    #Ambil data training dari database
+    X = hasilUKMPPD.objects.values('IPD', 'IKA', 'RAD', 'SRM', 'KDK', 'MPK', 'ANT', 'MAT', 'IKM', 'THTKL', 'KJW', 'OT2')
+    y = hasilUKMPPD.objects.values('hasil_ukmppd')
+    
+    return render(request, 'modelPrediksi/retrain-sem-3.html')
+
+def retrain_sem_4(request):
+    return render(request, 'modelPrediksi/retrain-sem-4.html')
